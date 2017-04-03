c403ba4ab0579fed96e9fbff39bb6bc881c1f71b
hemsingh1 <hemsingh23@gmail.com>
2017-04-03 12:58:25 +0530

    Update main.controller.js

--- main.controller.js
+++ main.controller.js
@@ -27,70 +27,10 @@ var app = angular.module('musicApp', ['ngRoute','spotify']);
       })
     };
 
-    // search an album
-   Spotify.search('Nirvana', 'artist').then(function (data) {
-    
-      console.log('=================== search results ===================');
-  console.log(data);
-});
-
-
-    Spotify.getAlbumTracks('41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
-      console.log('=================== Album Tracks - ID ===================');
-      console.log(data);
-    });
-    Spotify.getAlbumTracks('spotify:album:41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
-      console.log('=================== Album Tracks - Spotify URI ===================');
-      console.log(data);
-    });
 
 
 
-    //Artist
-    Spotify.getArtist('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist - Id ===================');
-      console.log(data);
-    });
-    Spotify.getArtist('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist - Spotify URI ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistAlbums('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist Albums - Id ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistAlbums('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Artist Albums - Spotify URI ===================');
-      console.log(data);
-    });
-
-    Spotify.getArtistTopTracks('0LcJLqbBmaGUft1e9Mm8HV', 'AU').then(function (data) {
-      console.log('=================== Artist Top Tracks Australia ===================');
-      console.log(data);
-    });
-
-    Spotify.getRelatedArtists('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
-      console.log('=================== Get Releated Artists ===================');
-      console.log(data);
-    });
-
-
-    //Tracks
-    Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function (data) {
-      console.log('=================== Track ===================');
-      console.log(data);
-    });
-
-    Spotify.getTracks('0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G').then(function (data) {
-      console.log('=================== Tracks - String ===================');
-      console.log(data);
-    });
-
-    Spotify.getTracks(['0eGsygTp906u18L0Oimnem','1lDWb6b6ieDQ2xT7ewTC3G']).then(function (data) {
-      console.log('=================== Tracks - Array ===================');
-      console.log(data);
+ 
     });
 
   }]);
