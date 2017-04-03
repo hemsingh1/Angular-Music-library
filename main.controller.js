dc93012b5b0e9f2defd52637cd31b2d77693fab8
hemsingh1 <hemsingh23@gmail.com>
2017-04-03 12:54:17 +0530

    Update main.controller.js

--- main.controller.js
+++ main.controller.js
@@ -13,7 +13,7 @@ var app = angular.module('musicApp', ['ngRoute','spotify']);
       Spotify.search($scope.searchartist, 'artist').then(function (data) {
         
       console.log('=================== Search results ===================');
-         console.log(data);
+         console.log($scope.searchartist);
         $scope.artists = data.artists.items;
       });
     };
