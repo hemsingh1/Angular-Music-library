fa67b5b9fb1780169372e1a1d7ad3ee7c8153bd5
hemsingh1 <hemsingh23@gmail.com>
2017-04-03 22:47:07 +0530

    Update main.controller.js

--- main.controller.js
+++ main.controller.js
@@ -30,7 +30,7 @@ var app = angular.module('musicApp', ['ngRoute','spotify']);
       });
         
     
-     $scope.artists = $scope.artists1.concat($scope.artists2);
+     $scope.artists = angular.merge($scope.artists1,$scope.artists2) ;
       
        
       
